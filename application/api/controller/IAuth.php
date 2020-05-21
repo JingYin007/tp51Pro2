35831a993e9b6b637c275e5643ad10b3f1d0adb4
moTzxx <JingYin007@users.noreply.github.com>
2020-05-21 20:06:01 +0800

    openssl_encrypt

--- application/api/controller/IAuth.php
+++ application/api/controller/IAuth.php
@@ -22,7 +22,8 @@ class IAuth
      * 根据注释，进行配置，提高系统安全性
      * @var string
      */
-    protected static $key = '12*()_3XXmoTzxx078734224';//自定义-注意保证24位字符串
+    protected static $key = '_7+()HU&&@^#@%8dd73sK';//自定义AES秘钥
+    protected static $iv = '170MT07**(&^#@6E';//自定义16位 AES偏移量
     protected static $set_pwd_pre_halt = '_#*moTzxx#mEx77BHGFSEDF'; //密码加密前缀
     protected static $session_cms_tag = 'cmsMoTzxxAID';//后台登录信息存储标记
     protected static $session_cms_scope = 'tp51Pro';// 后台登录信息存储 作用域
@@ -100,27 +101,9 @@ class IAuth
      * @return HexString
      */
     public static  function encrypt($input = '') {
-        @$size = mcrypt_get_block_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_ECB);
-        $input = self::pkcs5_pad($input, $size);
-        @$td = mcrypt_module_open(MCRYPT_RIJNDAEL_128, '', MCRYPT_MODE_ECB, '');
-        @$iv = mcrypt_create_iv(mcrypt_enc_get_iv_size($td), MCRYPT_RAND);
-        @mcrypt_generic_init($td, self::$key, $iv);
-
-        @$data = mcrypt_generic($td, $input);
-        @mcrypt_generic_deinit($td);
-        @mcrypt_module_close($td);
+        $data = openssl_encrypt($input, 'AES-256-CBC', self::$key, OPENSSL_RAW_DATA,self::$iv);
         $data = base64_encode($data);
         return $data;
-    }
-    /**
-     * 填充方式 pkcs5
-     * @param String text 		 原始字符串
-     * @param String blocksize   加密长度
-     * @return String
-     */
-    public static function pkcs5_pad($text, $blocksize) {
-        $pad = $blocksize - (strlen($text) % $blocksize);
-        return $text . str_repeat(chr($pad), $pad);
     }
 
     /**
@@ -130,11 +113,9 @@ class IAuth
      * @return String
      */
     public static function decrypt($sStr) {
-        @$decrypted= mcrypt_decrypt(MCRYPT_RIJNDAEL_128,self::$key,base64_decode($sStr), MCRYPT_MODE_ECB);
-        $dec_s = strlen($decrypted);
-        $padding = ord($decrypted[$dec_s-1]);
-        $decrypted = substr($decrypted, 0, -$padding);
-
+        $decrypted = openssl_decrypt(base64_decode($sStr), 'AES-256-CBC', self::$key, OPENSSL_RAW_DATA,self::$iv);
         return $decrypted;
     }
+
+
 }
