273f346bebc195f1f6156b414bb01c83ba568645
moTzxx <JingYin007@users.noreply.github.com>
2020-06-12 09:50:36 +0800

    ok

--- config/ftp.php
+++ config/ftp.php
@@ -9,7 +9,7 @@ return [
     'FTP_USE'=>'CLOSE', //是否启用ftp上传功能
     //TODO 本地测试一般不需要图片服务器 null，
     // 默认“/”,如果使用图片服务器，例：'http://img.xxxx.com/public/'
-    'IMG_SERVER_PATH'=>'',
+    'IMG_SERVER_PATH'=>'/',
     'IMG_SAVE_PATH'=>'/public/upload/',// FTP服务器中的图片存储路径
 
     'SERVER'=>'1xx.xx.x.xxx', //IP 如果满足内网IP条件，可大大提高传输效率
