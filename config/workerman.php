3c3429cb82f00f45db39017f48fa884a92c721a3
moTzxx <JingYin007@users.noreply.github.com>
2020-10-24 13:54:36 +0800

    ok

--- config/workerman.php
+++ config/workerman.php
@@ -10,5 +10,5 @@ return [
     //如果是 Windows 本地测试           举例 【ws://127.0.0.1:8282】
     //如果是 Linux服务器(http 协议)，    举例 【ws://139.xxx.x.xx:8282】
     //如果是 Linux服务器(https协议)，    举例 【wss://www.wenjiehorse.com/wss】
-    'WEB_SOCKET_URL' => 'ws://139.xxx.11.1:8282'  //web_socket 服务连接地址
+    'WEB_SOCKET_URL' => 'ws://139.224.1.31:8282'  //web_socket 服务连接地址
 ];
