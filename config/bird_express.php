40d8d4dc980d31bc5c44b4707cea504b8c057c04
moTzxx <JingYin007@users.noreply.github.com>
2020-11-12 17:25:14 +0800

    快递鸟
    补充设计 快递鸟物流 api

--- config/bird_express.php
+++ config/bird_express.php
@@ -2,11 +2,12 @@
 /**
  * Created by PhpStorm.
  * User: moTzxx
- * Date: 2019/8/7
+ * Date: 2020/11/12
  * Time: 11:11
  */
 return [
-    'E_BUSINESS_ID' =>'1273802',    //快递鸟注册 ID
-    'APP_KEY'  => 'a1e5f5f6-389e-48da-bccd-7ba29c8261b4',   //应用 APP_KEY
+    'E_BUSINESS_ID' =>'1xT07XM2',    //快递鸟注册 ID
+    'APP_KEY'  => 'miss#f##6-3-48da-?##d-7ba2YY????',   //应用 APP_KEY
+
     'REQUEST_URL' => "http://api.kdniao.com/Ebusiness/EbusinessOrderHandle.aspx",   //请求地址
 ];
