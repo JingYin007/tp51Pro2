d2efc44ed213c4d33e9b576c88b5a76785b8da1e
moTzxx <JingYin007@users.noreply.github.com>
2021-05-26 16:13:11 +0800

    Revert "优化验证码"
    This reverts commit efde36540402fad5e69b002faa5da02c828fb785.

--- config/captcha.php
+++ config/captcha.php
@@ -9,16 +9,13 @@
 return [
     // 验证码加密密钥
     'seKey'    => 'ThinkPHP.CN.moTzxx',
+
     // 验证码字符集合
-    'codeSet'  => '2347abdefhjkmnptuvwxACEFHJKLMNPRTUVWXY',
+    'codeSet'  => '2347abcdefhijkmnprtuvwxyACEFGHJKLMNPRTUVWXY',
 
     'length'   => 4,
     'fontSize' => 77,
     'useImgBg' => true,
     // 使用中文验证码
     'useZh'    => false,
-
-
-
-
     ];
