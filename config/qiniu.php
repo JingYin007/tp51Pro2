914deda0ddd64bd6c08626fe0f6429dd589a54bc
moTzxx <JingYin007@users.noreply.github.com>
2022-08-05 15:11:49 +0800

    TODO

--- config/qiniu.php
+++ config/qiniu.php
@@ -7,12 +7,12 @@
  */
 
 /**
- * 七牛云账号配置
+ * 七牛云账号配置 [TODO 请改成自己的账号数据]
  */
 return [
-    'QN_USE'=>'CLOSE',
+    'QN_USE'=>'OPEN',
     'AK' => 'SljXmBloaYvZRWyfvCQr3Z8ib6ljOWkUad6AMR2n',//Access_Key
     'SK' => 'gCO3lxjjfYFTyKFWW1J-oads1RdnoncDO8giQhFa',//Secret_Key
-    'BUCKET' => 'mz-think5api',//空间名称
-    'IMAGE_URL'=>'http://qak59ynet.bkt.clouddn.com/',//外链域名
+    'BUCKET' => 'mz-cheng',//空间名称
+    'IMAGE_URL'=>'http://rg4isdrjz.hb-bkt.clouddn.com/',//外链域名
 ];
